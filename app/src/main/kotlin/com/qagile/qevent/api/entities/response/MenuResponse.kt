0ef285ebf0167f5e0e840817d8e324dcaafe1157
Wagner Carvalho <wagner.carvalho@rappi.com>
2020-07-11 14:19:28 -0300

    code format

--- app/src/main/kotlin/com/qagile/qevent/api/entities/response/MenuResponse.kt
+++ app/src/main/kotlin/com/qagile/qevent/api/entities/response/MenuResponse.kt
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.qagile.qevent.api.domain.Menu
 import com.qagile.qevent.api.entities.CategoryStatus
 
-//data class MenuResponse (
+// data class MenuResponse (
 //
 //    @field:JsonProperty("data")
 //    var data: Category = Category()
 //
-//)
+// )
 
-data class MenuResponse (
+data class MenuResponse(
 
     @field:JsonProperty("food")
     var food: MutableList<Menu> = mutableListOf(),
@@ -23,11 +23,11 @@ data class MenuResponse (
     var others: MutableList<Menu> = mutableListOf()
 
 ) {
-    fun get(menuItems: MutableList<Menu>) : MenuResponse {
+    fun get(menuItems: MutableList<Menu>): MenuResponse {
 
         val menu = MenuResponse()
         menuItems.map {
-            when (it.category)  {
+            when (it.category) {
                 CategoryStatus.FOOD.name.toLowerCase() -> menu.food.add(it)
                 CategoryStatus.DRINK.name.toLowerCase() -> menu.drinks.add(it)
                 else -> menu.others.add(it)
